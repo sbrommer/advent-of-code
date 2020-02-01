f57a8cf0b6a7776798eafa9c3fe7b351162c2cfe
sbrommer <sbrommer@users.noreply.github.com>
2020-02-01 22:09:42 +0100

    2018/08 solution

---
+++ 2018/08/Main.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE TupleSections #-}
+
+module Main where
+
+import Data.Tree
+import Control.Arrow ((&&&))
+
+type Metadata = [Int]
+type Input = [Int]
+
+main :: IO()
+main = do
+    contents <- readFile "input.txt"
+    print $ (part1 &&& part2) $ parse contents
+
+parse :: String -> Tree Metadata
+parse = parseTree . map read . words
+
+parseTree :: Input -> Tree Metadata
+parseTree = head . fst . parseTree' . ([],)
+
+parseTree' :: ([Tree Metadata], Input) -> ([Tree Metadata], Input)
+parseTree' (siblings, c:m:input) = (tree : siblings, drop m input')
+    where
+        tree = Node (take m input') (reverse children)
+        (children, input') = applyNTimes c parseTree' ([], input)
+
+part1 :: Tree Metadata -> Int
+part1 = sum . fmap sum
+
+part2 :: Tree Metadata -> Int
+part2 tree@(Node metadata children)
+    | null children = part1 tree
+    | otherwise =
+          sum
+        $ map (part2 . (children !!) . subtract 1)
+        $ filter (`inRange` (1, length children)) metadata
+
+-- Helper functions
+inRange :: Int -> (Int, Int) -> Bool
+inRange n (l, h) = l <= n && n <= h
+
+applyNTimes :: Int -> (a -> a) -> a -> a
+applyNTimes n f x = iterate f x !! n
+
