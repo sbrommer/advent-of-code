6679d5f486d086ae394cda7970a381cde1904d20
sbrommer <sbrommer@users.noreply.github.com>
2021-11-30 17:42:34 +0100

    2015 day 9

---
+++ 2015/09/main.py
@@ -0,0 +1,29 @@
+from sys import stdin
+from itertools import permutations
+from parse import *
+
+# Parse input
+cities = set()
+dists = {}
+
+line = stdin.readline().strip()
+while len(line):
+    frm, to, dist = parse('{} to {} = {}', line)
+    dist = int(dist)
+
+    dists[(frm, to)] = dist
+    dists[(to, frm)] = dist
+
+    cities = cities.union([frm, to])
+
+    line = stdin.readline().strip()
+
+# Get total distance for each permutation
+total_dists = set()
+for p in permutations(cities):
+    route = zip(p, p[1:])
+    total_dists.add(sum(dists[r] for r in route))
+
+# Get answers
+print(min(total_dists))
+print(max(total_dists))
