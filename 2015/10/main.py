9cbaeb9189ccb187d3a384f809e399e6e5ce8ea0
sbrommer <sbrommer@users.noreply.github.com>
2021-12-01 11:30:03 +0100

    2015 day 10 refactoring

--- 2015/10/main.py
+++ 2015/10/main.py
@@ -1,23 +1,9 @@
-from sys import stdin
+from itertools import groupby
 
-x = stdin.readline().strip()
+x = open(0).readline().strip()
 
 def look_and_say(n):
-    count = 1
-    prev = n[0]
-    say = ''
-
-    for char in n[1:]:
-        if char == prev:
-            count += 1
-        else:
-            say += str(count) + prev
-            count = 1
-
-        prev = char
-
-    say += str(count) + prev
-    return say
+    return ''.join([str(len(list(g))) + c for c, g in groupby(n)])
 
 # Part 1
 for i in range(40):
