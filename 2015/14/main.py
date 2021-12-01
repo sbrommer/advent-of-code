62b2d2856cc011104cf7eb2cb428c40c2bc17bd0
sbrommer <sbrommer@users.noreply.github.com>
2021-12-01 21:41:51 +0100

    2015 day 14

---
+++ 2015/14/main.py
@@ -0,0 +1,28 @@
+from itertools import starmap
+from parse import findall
+
+T = 2503
+
+
+def distance(speed, t_fly, t_rest, t=T):
+    n, r = divmod(t, t_fly + t_rest)
+
+    return (n * t_fly + min(r, t_fly)) * speed
+
+
+# Parse data
+data = list(findall('{:d} km/s for {:d} seconds, but then must rest for {:d}',
+                    open(0).read()))
+
+# Part 1
+print(max(starmap(distance, data)))
+
+# Part 2
+races = [[distance(*deer, t+1) for t in range(T)] for deer in data]
+snapshots = zip(*races)
+
+winners = []
+for s in snapshots:
+    winners += [deer for deer, dist in enumerate(s) if dist == max(s)]
+
+print(max([winners.count(w) for w in set(winners)]))
