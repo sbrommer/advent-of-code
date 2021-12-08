0ac5f74223724b70444d6c9876338be81e0fd52d
sbrommer <sbrommer@users.noreply.github.com>
2021-12-08 14:10:48 +0100

    2015 day 21

--- 2015/21/main.py
+++ 2015/21/main.py
@@ -1,7 +1,7 @@
-from parse import search
-from math import ceil
-import numpy as np
 from itertools import product
+from math import ceil
+from parse import search
+import numpy as np
 
 # Shop
 ws = np.array([[8, 4, 0], [10, 5, 0], [25, 6, 0], [40, 7, 0], [74, 8, 0]])
@@ -9,7 +9,7 @@ am = np.array([[0, 0, 0], [13, 0, 1], [31, 0, 2], [53, 0, 3], [75, 0, 4], [102, 
 rs = np.array([[0, 0, 0], [25, 1, 0], [50, 2, 0], [100, 3, 0], [20, 0, 1], [40, 0, 2], [80, 0, 3]])
 
 sets = [sum(s) for s in product(ws, am, rs, rs)
-        if not all(s[2] == s[3]) or all(s[2] == rs[0,0])]
+        if not all(s[2] == s[3]) or all(s[2] == rs[0, 0])]
 
 # Parse boss
 hp, dmg, amr = np.array(list(search('Hit points: {:d}\nDamage: {:d}\nArmor: {:d}', open(0).read())))
