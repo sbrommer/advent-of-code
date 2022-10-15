868339ba55fc9b8a76f9dba897d0d6a1e9e51060
Susan <sbrommer@users.noreply.github.com>
2022-10-15 13:12:13 +0200

    2016 day 19

---
+++ 2016/19/main.py
@@ -0,0 +1,23 @@
+def part1(n):
+    b = format(n, 'b')
+    w = b[1:] + '1'
+    return int(w, 2)
+
+
+def part2(n):
+    i = 1
+    while i * 3 < n:
+        i *= 3
+
+    if i < n <= 2 * i:
+        w = n - i
+    else:
+        w = 2 * n - 3 * i
+
+    return w
+
+
+n = 3001330
+
+print(part1(n))
+print(part2(n))
