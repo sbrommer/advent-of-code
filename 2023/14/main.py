30512f4863359a6ad3b7f63017f158c917ad3ae5
Susan <sbrommer@users.noreply.github.com>
2023-12-15 13:45:08 +0100

    2023 day 14 refactoring

--- 2023/14/main.py
+++ 2023/14/main.py
@@ -3,8 +3,8 @@ def transpose(platform):
 
 
 def tilt(platform, rev=True):
-    return tuple('#'.join(''.join(sorted(rock, reverse=rev))
-                 for rock in row.split('#')) for row in platform)
+    return ['#'.join(''.join(sorted(rock, reverse=rev))
+            for rock in row.split('#')) for row in platform]
 
 
 def load(platform):
@@ -23,17 +23,18 @@ platform = [line.strip() for line in open(0)]
 # Part 1
 print(load(tilt(transpose(platform))))
 
+
 # Part 2
-n = 1_000_000_000
-cache = {tuple(platform): 0}
+def solve(platform):
+    n = 1_000_000_000
+    cache = []
 
-for i in range(n):
-    platform = cycle(platform)
-    if platform not in cache:
-        cache[platform] = i
-    else:
-        for _ in range((n - i) % (i - cache[platform]) - 1):
-            platform = cycle(platform)
-        break
+    while platform not in cache:
+        cache.append(platform)
+        platform = cycle(platform)
 
-print(load(transpose(platform)))
+    i = cache.index(platform)
+    return cache[i + (n - i) % (len(cache) - i)]
+
+
+print(load(transpose(solve(platform))))
