65c4b69e5dddc812df11054f0b42716d91ce5ee1
Susan <sbrommer@users.noreply.github.com>
2023-12-19 20:55:15 +0100

    2015 day 21 refactoring

--- 2015/21/main.py
+++ 2015/21/main.py
@@ -9,10 +9,11 @@ am = np.array([[0, 0, 0], [13, 0, 1], [31, 0, 2], [53, 0, 3], [75, 0, 4], [102, 
 rs = np.array([[0, 0, 0], [25, 1, 0], [50, 2, 0], [100, 3, 0], [20, 0, 1], [40, 0, 2], [80, 0, 3]])
 
 sets = [sum(s) for s in product(ws, am, rs, rs)
-        if not all(s[2] == s[3]) or all(s[2] == rs[0, 0])]
+        if not all(s[2] == s[3]) or all(s[2] == [0, 0, 0])]
 
 # Parse boss
 hp, dmg, amr = [*search('Hit points: {:d}\nDamage: {:d}\nArmor: {:d}', open(0).read())]
+
 Hp = 100
 
 
