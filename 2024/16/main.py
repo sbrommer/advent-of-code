9279f4ca25ed375ffbab5351d73ce59ae1c1f2d6
Susan <sbrommer@users.noreply.github.com>
2024-12-25 11:35:41 +0100

    refactor 2024 day 16

--- 2024/16/main.py
+++ 2024/16/main.py
@@ -7,14 +7,15 @@ grid = {x+y*1j: c
 
 E, = [p for p, c in grid.items() if c == 'E']
 S, = [p for p, c in grid.items() if c == 'S']
-G  = [p for p, c in grid.items() if c != '#']
+G  = {p for p, c in grid.items() if c != '#'}
 
 # Solve
-seen, q = set(), [(0, i := 0, S, 1, {S})]
+seen, q = set(), [(0, i:=0, [S], 1)]
 best, tiles = 1e9, set()
 
 while q:
-    c, _, p, d, path = pop(q)
+    c, _, path, d = pop(q)
+    p = path[-1]
 
     if c > best:
         break
@@ -22,13 +23,13 @@ while q:
     if p == E:
         if c < best:
             best, tiles = c, set()
-        tiles |= path
+        tiles |= {*path}
 
-    for dc, r in [(1, 1), (1001, 1j), (1001, -1j), (2001, -1)]:
+    for dc, r in [(1, 1), (1001, 1j), (1001, -1j)]:
         d_new = d*r
         p_new = p+d_new
         if p_new in G and (p_new, d_new) not in seen:
-            push(q, (c+dc, i:=i+1, p_new, d_new, path | {p_new}))
+            push(q, (c+dc, i:=i+1, path+[p_new], d_new))
 
     seen |= {(p, d)}
 
