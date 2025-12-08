903aa1de636051ca6196bb4469b3574b440db8b6
Suus <->
2025-12-08 17:44:51 +0100

    refactor 2025 day 7

--- 2025/07/main.py
+++ 2025/07/main.py
@@ -4,19 +4,11 @@ splits = 0
 beams = [c == 'S' for c in start]
 
 for line in diagram[::2]:
-    new_beams = [0] * len(beams)
-
     for i in range(len(beams)):
-        if beams[i]:
-            match line[i]:
-                case '.':
-                    new_beams[i] += beams[i]
-
-                case '^':
-                    splits += 1
-                    new_beams[i-1] += beams[i]
-                    new_beams[i+1] += beams[i]
-
-    beams = new_beams
+        if beams[i] and line[i] == '^':
+            splits += 1
+            beams[i-1] += beams[i]
+            beams[i+1] += beams[i]
+            beams[i] = 0
 
 print(splits, sum(beams))
